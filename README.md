d7f56f6193df154f3db6e5fd35679fa077990a6f
diogoffmelo <diogoffmelo@gmail.com>
2018-12-15 19:07:21 -0300

    Adiciona estrutura do crawler e Makefile.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Crawler do site de filmes [IMDB](https://www.imdb.com/).
 
 # TODO list
 [x] - Criar repositório
+    [x] - Criar módulo e setup.
 
 [ ] - Craweler
 
---
+++ imdb/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(name='imdb',
+      version='0.0.1',
+      description='crawler + servicos para o site IMDB.',
+      url='https://github.com/diogoffmelo/imdb_crawler',
+      author='diogoffmelo',
+      author_email='diogoffmelo@gmail.com',
+      packages=['imdb'],
+)
